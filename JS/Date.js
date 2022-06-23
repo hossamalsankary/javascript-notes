fc3add60fe4f54b80987095b06a6d26268f29a1c
hossam Alsankary <hossamalsankary@gmail.com>
2022-06-23 15:56:07 +0300

    Start JS RoudMap

--- JS/Date.js
+++ JS/Date.js
@@ -111,3 +111,17 @@ var utcDate = new Date(Date.UTC(2000, 0, 31, 12));
 var localDate = new Date(2000, 0, 31, 12);
 log(localDate - utcDate === utcDate.getTimezoneOffset() * 60 * 1000);
 
+
+// Date Comparison
+// Comparing Date values
+var date1 = new Date();
+var date2 = new Date(date1.valueOf() + 1000);
+log(date1 < date2);
+log(date1.getTime(), "==", date1.valueOf());
+
+//To compare the diﬀerence of two dates, we can do the comparison based on the timestamp.
+var date1 = new Date();
+var date2 = new Date(date1.valueOf() + 5000);
+var dateDiff = date1.valueOf() - date2.valueOf();
+var dateDiffInYears = dateDiff/1000/60/60/24/365; //convert milliseconds into years
+log("Date difference in years : " + dateDiffInYears);
